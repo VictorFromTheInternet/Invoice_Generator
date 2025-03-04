2a93cb4444df2d39ff78d2dbfdd315bd83a39cd9
VictorFromTheInternet <vma0430@gmail.com>
2025-03-04 13:02:29 -0600

    displaying the pdf with an iframe on the front end

--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -11,7 +11,24 @@ async function generateInvoice(formData){
     })
 
     const pdfData = await response.json()
+    showPDF(pdfData.dataUrl)
     console.log(pdfData)
+}
+
+async function showPDF(dataUrl){
+
+
+    const pdfContainer = document.querySelector('.pdf-container')
+
+    let iframePdf = document.createElement('iframe')
+    iframePdf.src = dataUrl
+    iframePdf.height = 480
+    iframePdf.width = 720
+
+    console.log(iframePdf)
+    console.log(pdfContainer)
+
+    pdfContainer.appendChild(iframePdf)
 }
 
 document.querySelector('#btnDemo').addEventListener('click', ()=>{
