38303a519b1a98147a49d3107f5a3b9757886606
VictorFromTheInternet <vma0430@gmail.com>
2025-03-04 11:03:41 -0600

    adjusted the logging timestamp to central time

--- backend/middleware/logging.js
+++ backend/middleware/logging.js
@@ -4,7 +4,17 @@ const path = require('path');
 const filePath = path.join(__dirname, "..", "logging", "logging.txt");
 
 module.exports = async(req,res,next)=>{
-    const timestamp = new Date().toUTCString()
+    const timestamp = new Date().toLocaleString("en-US",{
+        weekday: "short",
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZone: "CST",
+        timeZoneName: "short"
+    })
     const method = req.method;
     const url = req.url;
     const data = JSON.stringify(req.body)
