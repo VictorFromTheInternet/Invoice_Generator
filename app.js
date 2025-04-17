c67ed01b4ffd9df9d95f07c429bdfcad32dc46c6
VictorFromTheInternet <vma0430@gmail.com>
2025-04-16 19:33:30 -0500

    async connect to db

--- app.js
+++ app.js
@@ -8,36 +8,42 @@ const port = process.env.PORT || 3000
 const domain = (process.env.NODE_ENV == 'prod') ? process.env.DOMAIN : 'http://localhost'
 const MONGO_URI = process.env.MONGO_URI
 
-// db connection
-console.log(`Mongo URI: ${MONGO_URI}`)
-console.log(`Domain: ${domain}`)
-mongoose.connect(MONGO_URI,{
-    serverSelectionTimeoutMS: 30000 // Increase timeout
-  })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err))
 
-// routers
-const views_router = require('./routes/views_router')
-const pdf_router = require('./routes/pdf_router')
-const submission_router = require('./routes/submission_router')
-const logger = require('./middleware/logging')
-
-// middleware
-app.use(express.json({limit: '50mb'}))
-app.use(express.urlencoded({extended: true}))
-// app.use('/', logger)
-
-// routes
-app.use(express.static(path.join(__dirname, "public")));
-app.use('/', views_router)
-app.use('/', pdf_router)
-app.use('/invoice', submission_router)
+async function startServer(){
+    try{
+        // db connection
+        console.log(`Mongo URI: ${MONGO_URI}`)
+        console.log(`Domain: ${domain}`)
+        await mongoose.connect(MONGO_URI,{
+            serverSelectionTimeoutMS: 30000 // Increase timeout
+        })
+        console.log('Connected to DB')
 
 
+        // routers
+        const views_router = require('./routes/views_router')
+        const pdf_router = require('./routes/pdf_router')
+        const submission_router = require('./routes/submission_router')
+        const logger = require('./middleware/logging')
 
+        // middleware
+        app.use(express.json({limit: '50mb'}))
+        app.use(express.urlencoded({extended: true}))        
 
-app.listen(port, ()=>{
-    console.log(`Server Started`)
-    console.log(`Listening on : ${domain}:${port}`)
-})
+        // routes
+        app.use(express.static(path.join(__dirname, "public")));
+        app.use('/', views_router)
+        app.use('/', pdf_router)
+        app.use('/invoice', submission_router)
+
+        // listen
+        app.listen(port, ()=>{
+            console.log(`Server Started`)
+            console.log(`Listening on : ${domain}:${port}`)
+        })
+
+    }catch(err){
+        console.error('MongoDB connection error:', err)
+    }                
+}
+startServer()
