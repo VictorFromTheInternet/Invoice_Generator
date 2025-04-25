8bbb307a18582ea92a1f83d1381b5abc83f20b5c
VictorFromTheInternet <vma0430@gmail.com>
2025-04-25 12:05:29 -0500

    removing form

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
 const express = require('express')
-const mongoose = require('mongoose')
 require('dotenv').config()
 const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 3000
 const domain = (process.env.NODE_ENV == 'prod') ? process.env.DOMAIN : 'http://localhost'
-const MONGO_URI = process.env.MONGO_URI
 
 
 async function startServer(){
     try{
-        // db connection
-        console.log(`Mongo URI: ${MONGO_URI}`)
-        console.log(`Domain: ${domain}`)
-        await mongoose.connect(MONGO_URI)
-        console.log('Connected to DB')
 
 
-        // routers
-        const views_router = require('./routes/views_router')
-        const pdf_router = require('./routes/pdf_router')
-        const submission_router = require('./routes/submission_router')
-        const logger = require('./middleware/logging')
+        // routers        
+        const pdf_router = require('./routes/pdf_router')        
 
         // middleware
         app.use(express.json({limit: '50mb'}))
         app.use(express.urlencoded({extended: true}))        
 
         // routes
-        app.use(express.static(path.join(__dirname, "public")));
-        app.use('/', views_router)
-        app.use('/', pdf_router)
-        app.use('/invoice', submission_router)
+        app.use(express.static(path.join(__dirname, "public")));        
+        app.use('/', pdf_router)        
 
         // listen
         app.listen(port, ()=>{
