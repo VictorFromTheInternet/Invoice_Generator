82ad6231b5a9b98cce0ccf2b3f944986b1dbb92b
VictorFromTheInternet <vma0430@gmail.com>
2025-04-26 11:48:14 -0500

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,6 +18,8 @@ async function startServer(){
         // middleware
         app.use(express.json({limit: '50mb'}))
         app.use(express.urlencoded({extended: true}))        
+        app.use(cors())
+        app.options('/generate-pdf', cors());
 
         // routes
         app.use(express.static(path.join(__dirname, "public")));        
